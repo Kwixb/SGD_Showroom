0546e0bb29ca7e081a79baaaf002e66e6575a30f
evykol <evykol@users.noreply.github.com>
2018-03-30 19:22:02 -0400

    EvysChanges

---
+++ SGD_Showroom/Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMovie : MonoBehaviour
+{
+
+    void Update ()
+    {
+		if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Renderer r = GetComponent<Renderer>();
+            MovieTexture movie = (MovieTexture)r.material.mainTexture;
+            movie.loop = true;
+
+            if (movie.isPlaying)
+            {
+                movie.Pause();
+            }
+            else
+            {
+                movie.Play();
+            }
+        }
+        
+
+    }
+}
