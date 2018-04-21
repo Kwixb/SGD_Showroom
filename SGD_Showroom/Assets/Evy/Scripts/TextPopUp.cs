a6ee5ca6e4525ebd7856644fb3274f3f9b49d6c3
evykol <evykol@users.noreply.github.com>
2018-04-20 20:13:51 -0400

    EvyPopUpWindowUI

---
+++ SGD_Showroom/Assets/Evy/Scripts/TextPopUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPopUp : MonoBehaviour
+{
+    public GameObject informationPopUp;
+
+    private void Start()
+    {
+        informationPopUp.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            informationPopUp.SetActive(false);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" )
+        {
+                informationPopUp.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        informationPopUp.SetActive(false);
+    }
+
+
+}
